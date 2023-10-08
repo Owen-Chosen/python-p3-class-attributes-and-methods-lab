f9c192837380ab1b7a8d76bd04867882a4780958
owen <owen.chosen@gmail.com>
2023-10-08 20:30:35 +0100

    Done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,45 @@
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+    names = []
+    genre_count = {}
+    artist_count = {}
+    genre_no = 0
+    artist_no = 0
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_artists(artist)
+        self.add_to_genres(genre)
+        # self.add_to_genre_count(genre)
+
+
+    @classmethod
+    def add_song_to_count(cls, increment = 1):
+        cls.count+=increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre in cls.genres:
+            return
+        else:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist in cls.artists:
+            return
+        else:
+            cls.artists.append(artist)
+
+    # @classmethod
+    # def add_to_genre_count(cls, genre):
+        # cls.genre_count[genre] = (int(cls.genre_count[genre])+1)
+
+# song = Song("yawa", "tecno", "afro")
+# print(Song.genre_count)
